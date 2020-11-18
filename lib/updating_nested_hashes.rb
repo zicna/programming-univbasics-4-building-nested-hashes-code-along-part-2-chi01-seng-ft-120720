798d419f41e8642fb7916edec5e812cdf3a4de68
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 20:47:59 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,7 +22,7 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
-  epic_tragedy[:montague][:hero].status: = "dead"
+  epic_tragedy[:montague][:hero].status = "dead"
   epic_tragedy[:capulet][:heroine][status:] = "dead"
 
 
